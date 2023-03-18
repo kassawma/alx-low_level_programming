04c095f03ffd628e23e225a11b36b38ba95097e9
kassawma <markonayalew017@gmail.com>
2023-03-18 07:06:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print number 0 to 9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int k;
+for (k = 48; k < 58; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
