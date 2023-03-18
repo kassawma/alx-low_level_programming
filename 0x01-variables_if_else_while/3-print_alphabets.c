1fb602ad4b7b911fe2fde0d36669c9249ed2aba6
kassawma <markonayalew017@gmail.com>
2023-03-18 06:01:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print english lowercase and upper case Alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alpah[52] = "abcdefghijklmnopqrstuvwxyz";
+int b;
+for (b = 0; b < 52; b++)
+{
+putchar(alpah[b]);
+}
+putchar('\n');
+return (0);
+}
