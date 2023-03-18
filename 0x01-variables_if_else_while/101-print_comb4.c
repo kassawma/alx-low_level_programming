7020ebe8319e272e01ee6fe31030f7d502d4a0ed
kassawma <markonayalew017@gmail.com>
2023-03-18 12:00:20 -0700

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - print three compositions
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int k, a, s;
+for (k = 48; k < 58; k++)
+{
+for (a = 49; a < 58; a++)
+{
+for (s = 50; s < 58; s++)
+{
+if (k > a && a > s)
+{
+putchar(k);
+putchar(a);
+putchar(s);
+if (k != 55 || a != 56)
+{
+putchar(',');
+putchar(' ');
+}}}}}
+putchar('\n');
+return (0);
+}
