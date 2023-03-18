db6271319285ac44958f945f9acf78a881ce400a
kassawma <markonayalew017@gmail.com>
2023-03-18 06:35:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - print a number 0 to 9
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int k;
+for (k = 0; k < 10; k++)
+{
+printf("%d", k);
+}
+return (0);
+}
