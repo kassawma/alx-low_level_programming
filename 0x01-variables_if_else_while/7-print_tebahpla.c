517529b1bf07df5634a528ebdcffee200466711a
kassawma <markonayalew017@gmail.com>
2023-03-18 09:57:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*  main - print mirror or reverse
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
