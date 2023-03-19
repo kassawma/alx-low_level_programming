058eac135489202e945b4bf755f8bd3a6a36253b
kassawma <markonayalew017@gmail.com>
2023-03-19 10:33:21 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*  main - print  two number two
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int k, l;
+for (k = 0; k < 100; k++)
+{
+for (l = 0; l < 100; l++)
+{
+if (k < l)
+{
+putchar((k / 10) + 48);
+putchar((k % 10) + 48);
+putchar(' ');
+putchar((l / 10) + 48);
+putchar((l % 10) + 48);
+if (k != 98 || l != 99)
+{
+putchar(',');
+putchar(' ');
+}}}}
+putchar('\n');
+return (0);
+}
