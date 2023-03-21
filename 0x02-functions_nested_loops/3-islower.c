f49263c0375dbca43b5777251c461d93fb8947b7
kassawma <markonayalew017@gmail.com>
2023-03-21 14:06:53 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - is check
+* @c: is check character
+* Return: 1 is secusss
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
