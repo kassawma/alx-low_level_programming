0a057e066ddb09a7efbc9cb788359941a0631f66
kassawma <markonayalew017@gmail.com>
2023-03-21 14:52:46 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_sign - is sig
+* @n: is number
+* Return: 1 and + sign
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - absolute values
+* @n: number
+* Return:0 is success
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
