dccf9437364dbc884425f3dc5f2d41bab846278b
kassawma <markonayalew017@gmail.com>
2023-03-22 10:12:58 -0700

    thank you

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,9 @@
 */
 void print_to_98(int n)
 {
-int i, j;
 if (n <= 98)
 {
+int i;
 for (i = n; i <= 98; i++)
 {
 if (i != 98)
@@ -18,7 +18,7 @@ printf("%d\n", i);
 }}
 else if (n >= 98)
 {
-
+int j;
 for (j = n; n >= 98; j--)
 {
 if (j != 98)
