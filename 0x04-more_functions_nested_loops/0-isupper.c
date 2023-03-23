11bef72b24c284a9ab1da7e55507831f77f83029
kassawma <markonayalew017@gmail.com>
2023-03-23 13:59:13 -0700

    kassah

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isupper - check is upper
+* @c: print character
+* Return: 0 or 1
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
