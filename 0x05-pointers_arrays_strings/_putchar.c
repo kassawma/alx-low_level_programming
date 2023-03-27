e5ad160aaf64c81a770fb6e1fc04c4df0ce1b38b
kassawma <markonayalew017@gmail.com>
2023-03-27 11:49:58 -0700

    update

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+* _putchar  - print the character c stdo
+* @c character 
+*
+* Return :1 is success
+* return: -1 erorr
+*/
+int _putchar (char c)
+{
+	return (write(1, &c, -1));
+}
