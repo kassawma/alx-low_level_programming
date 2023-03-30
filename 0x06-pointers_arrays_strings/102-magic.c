fe8ada0bfe49fa5d6ffd8433e468689200859ff1
kassawma <markonayalew017@gmail.com>
2023-03-30 12:33:29 -0700

    update

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+*(p + 5) = 98;
+/*... so that print 98\n */
+print (*a[2] = %d\n", a[2]);
+return (0);
+}
