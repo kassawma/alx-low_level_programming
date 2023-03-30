6e17206166ddca9434b0b5b706a3833e9f798a32
kassawma <markonayalew017@gmail.com>
2023-03-30 13:04:02 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,6 +8,7 @@ char *cap_string(char *str)
 {
 int i = 0;
 while (str[i])
+{
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
 if (str[i - 1] == ' ' ||
@@ -20,5 +21,6 @@ str[i - 1] == '\t' || str[i - 1] == '\n'
 || i == 0)
 str[i] -= 32;
 i++;
+}
 return (str);
 }
