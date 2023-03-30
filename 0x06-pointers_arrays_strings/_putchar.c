cc62bccbeeb9a4784c7ec24abe801be71ce93a4c
kassawma <markonayalew017@gmail.com>
2023-03-30 12:24:46 -0700

    update

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar - put char 
+* @n: print the character
+* Return: 0 succes
+* On error: -1
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
