fa41839138f7bd24b4d3a6a13533c750be7d3d19
kassawma <markonayalew017@gmail.com>
2023-04-05 12:26:06 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _pow_recursion - power recursion
+* @x: rasie
+* @y: power
+* Return: return the power
+*/
+int _pow_recursion(int x, int y)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (x * factorial(x, y - 1));
+}
