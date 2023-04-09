0d14326144f8e9e67f193aef819ab41fd821cf37
kassawma <markonayalew017@gmail.com>
2023-04-09 12:04:33 -0700

    you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - mainfunction
+* @argc: argument
+* @argv: arangmennt
+* Return: success
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
