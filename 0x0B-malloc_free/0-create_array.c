4f0c2a857f4ace8e71b1bf830de99f018b916b70
kassawma <markonayalew017@gmail.com>
2023-04-11 13:17:53 -0700

    dete

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 char *str;
 unsigned int i;
 str = malloc(sizeof(char) * size);
-if (i == 0 || str == NULL)
+if (size == 0 || str == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 str[i] = c;
